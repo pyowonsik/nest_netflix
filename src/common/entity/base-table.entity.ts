26fa194e3f5ae0295eb3eb5f44166918895cf8db
표원식 <pyowonsik@pyowonsig-ui-MacBookPro.local>
2024-12-14 15:37:36 +0900

    refactor : 프론트엔드에서 필요없는값 exclue 처리

--- src/common/entity/base-table.entity.ts
+++ src/common/entity/base-table.entity.ts
@@ -1,3 +1,4 @@
+import { Exclude } from 'class-transformer';
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
 // 현재는 Entity Inharitance 방식을 사용했지만 , 필요하다면 Entity Embeding , Single Table Inheritance 학습
@@ -5,11 +6,14 @@ import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 // Entity Inharitance
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude() // 프론트엔드에서 필요없는 값 숨김 처리
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude() // 프론트엔드에서 필요없는 값 숨김 처리
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude() // 프론트엔드에서 필요없는 값 숨김 처리
   version: number;
 }
